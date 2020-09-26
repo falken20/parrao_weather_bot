59a90e4b9a7c668bd0830c0ca109c45e5ada7cf9
Richi <ricardorg20@gmail.com>
2020-09-26 23:51:04 +0200

    First commit

---
+++ config_fk.py
@@ -0,0 +1,25 @@
+# by Richi Rod AKA @richionline / falken20
+
+__title__ = 'Richi Rod Portfolio'
+__version__ = '1.0.0'
+__author__ = 'Falken'
+__url_github__ = 'https://github.com/falken20/'
+__url_twitter__ = 'https://twitter.com/richionline'
+__url_linkedin__ = 'https://www.linkedin.com/in/richionline/'
+__license__ = 'MIT License'
+__copyright__ = '© 2020 by Richi Rod AKA @richionline / falken20'
+__features__ = [
+]
+
+
+SETUP_DATA = {
+    'title': __title__,
+    'version': __version__,
+    'author': __author__,
+    'url_github': __url_github__,
+    'url_twitter': __url_twitter__,
+    'url_linkedin': __url_linkedin__,
+    'license': __license__,
+    'copyrigth': __copyright__,
+    'features': __features__,
+}
