fffd7ed18363e5e226016bfe91b55d04f597b809
Richi <r.rodriguez@bbva.com>
2023-04-20 07:58:18 +0200

    Set up cloudbuild.yaml

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 
-# LOAD PROPERTIES
-#. version.properties
-
 echo ">>> Environment vars"
 echo _SERVICE_ACCOUNT: $_SERVICE_ACCOUNT
-echo _BBVA_GAE_ENV: $_BBVA_GAE_ENV
-echo _TOPIC_NAME: $_TOPIC_NAME
 
 echo ">>> Delete build folder"
 rm -rf build
@@ -14,40 +9,32 @@ rm -rf build
 echo ">>> Create build folder and subfolders"
 mkdir build
 mkdir build/src
-mkdir build/src/send_sigma
-mkdir build/src/init_sigma
+mkdir build/src/parrao_weather_bot
 
-echo ">>> Copy files to build/src/send_sigma"
-cp -R src/send_sigma build/src
-echo ">>> Copy files to build/src/init_sigma"
-cp -R src/init_sigma build/src
+echo ">>> Copy files to build/src/parrao_weather_bot"
+cp -R main.py build/src
+cp -R parrao_weather_bot build/src
 
 # Local execution
 #export _SERVICE_ACCOUNT=cloud--functions@dev-bbva-work-space-booking-sp.iam.gserviceaccount.com
-#export _BBVA_GAE_ENV=dev
 
-echo ">>> Folder content send_sigma:"
-ls build/src/send_sigma
-echo ">>> Folder content init_sigma:"
-ls build/src/init_sigma
-
+echo ">>> Folder content parrao_weather_bot:"
+ls build/src/parrao_weather_bot
 echo ">>> Deploy cf-send_sigma Cloud Function. Current folder:"
-cd build/src/send_sigma
+cd build/src/parrao_weather_bot
 pwd
-gcloud functions deploy cf-send_sigma --region=europe-west1 \
-    --runtime=python39 --entry-point=send_sigma --memory=128MB --timeout=120s \
-    --trigger-topic=$_TOPIC_NAME \
-    --env-vars-file ./env-$_BBVA_GAE_ENV.yaml \
-    --service-account=$_SERVICE_ACCOUNT
 
-echo ">>> Deploy cf-init_sigma Cloud Function. Current folder:"
-cd ../../../build/src/init_sigma
-pwd
-gcloud functions deploy cf-init_sigma --region=europe-west1 \
-    --runtime=python39 --entry-point=init_sigma --memory=128MB --timeout=120s \
-    --trigger-http \
-    --env-vars-file ./env-$_BBVA_GAE_ENV.yaml \
-    --service-account=$_SERVICE_ACCOUNT
+gcloud functions deploy cercedilla-weather-bot --region=europe-west1 \
+    --runtime=python37 --entry-point=parrao_weather_bot --memory=128MB --timeout=120s \
+    --env-vars-file ./credentials.yaml \
+    --service-account=$_SERVICE_ACCOUNT \
+    --trigger-http --source=.
+
+gcloud functions deploy cercedilla-weather-daily-bot --region=europe-west1 \
+    --runtime=python37 --entry-point=parrao_weather_bot --memory=128MB --timeout=120s \
+    --env-vars-file ./credentials.yaml \
+    --service-account=$_SERVICE_ACCOUNT \
+    --trigger-http --source=.
 
 echo ">>> Clean and delete build folder"
 cd ../../..
